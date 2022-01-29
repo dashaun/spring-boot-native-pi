ecb3088a5c94a649952e6ead18beacf761d2abd3
DaShaun <dashaun@dashaun.com>
2022-01-29 16:24:29 -0600

    first pass

---
+++ src/main/java/dev/dashaun/springpi/SpringPiApplication.java
@@ -0,0 +1,13 @@
+package dev.dashaun.springpi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/dashaun/springpi/SpringPiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dashaun.springpi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
